05a8fbcec28456c0153fa4a5602e1b8e343ce893
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-01-15 15:16:04 -0300

    configs

---
+++ src/config/fonts.ts
@@ -0,0 +1,11 @@
+import { Inter, Montserrat_Alternates } from "next/font/google";
+
+export const font = Montserrat_Alternates({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+export const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
