f8f6eba16ab06921a5d6f203abce81cbf3d528f7
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-01-23 05:48:48 -0300

    feat: manejamos el error por si no se encuentran los productos

---
+++ src/app/(shop)/error.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import Image from 'next/image';
+
+export default function error() {
+  return (
+    <div className="flex h-[80dvh] items-center justify-center">
+      <div className="text-center">
+        <Image
+          src="/imgs/starman_750x750.png" // Reemplaza con la ruta correcta de tu imagen
+          alt="Error Image"
+          width={1000}
+          height={1000}
+          className="mx-auto mb-4 w-72"
+        />
+        <h1 className="mb-2 text-2xl font-semibold">
+          Sitio Temporalmente Cerrado
+        </h1>
+        <p className="text-gray-600">
+          Estamos realizando mejoras. Vuelve pronto.
+        </p>
+      </div>
+    </div>
+  );
+}
