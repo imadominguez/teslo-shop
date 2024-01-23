ec881ce8a7d8a9dfe721b9227ce450a22ea3da19
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-01-23 04:42:52 -0300

    cambio de altura de sliders del producto

--- src/components/product/slideshow/ProductMobileSlideshow.tsx
+++ src/components/product/slideshow/ProductMobileSlideshow.tsx
@@ -1,14 +1,14 @@
-"use client";
+'use client';
 
-import Image from "next/image";
-import { Swiper, SwiperSlide } from "swiper/react";
+import Image from 'next/image';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
-import "swiper/css";
-import "swiper/css/free-mode";
-import "swiper/css/pagination";
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/pagination';
 
-import "./slideshow.css";
-import { Autoplay, FreeMode, Navigation, Pagination } from "swiper/modules";
+import './slideshow.css';
+import { Autoplay, FreeMode, Navigation, Pagination } from 'swiper/modules';
 
 interface Props {
   images: string[];
@@ -20,8 +20,8 @@ export const ProductMobileSlideshow = ({ images, title, className }: Props) => {
     <div className={className}>
       <Swiper
         style={{
-          width: "100vw",
-          height: "500px",
+          width: '100vw',
+          height: '42dvh',
         }}
         pagination
         autoplay={{
--- src/components/product/slideshow/ProductSlideshow.tsx
+++ src/components/product/slideshow/ProductSlideshow.tsx
@@ -1,16 +1,16 @@
-"use client";
-import { useState } from "react";
-import { Swiper as SwiperObject } from "swiper";
-import { Swiper, SwiperSlide } from "swiper/react";
+'use client';
+import { useState } from 'react';
+import { Swiper as SwiperObject } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
-import "swiper/css";
-import "swiper/css/free-mode";
-import "swiper/css/navigation";
-import "swiper/css/thumbs";
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/navigation';
+import 'swiper/css/thumbs';
 
-import "./slideshow.css";
-import { Autoplay, FreeMode, Navigation, Thumbs } from "swiper/modules";
-import Image from "next/image";
+import './slideshow.css';
+import { Autoplay, FreeMode, Navigation, Thumbs } from 'swiper/modules';
+import Image from 'next/image';
 
 interface Props {
   images: string[];
@@ -25,8 +25,9 @@ export const ProductSlideshow = ({ images, title, className }: Props) => {
       <Swiper
         style={
           {
-            "--swiper-navigation-color": "#fff",
-            "--swiper-pagination-color": "#fff",
+            '--swiper-navigation-color': '#fff',
+            '--swiper-pagination-color': '#fff',
+            height: '40dvh',
           } as React.CSSProperties
         }
         spaceBetween={10}
