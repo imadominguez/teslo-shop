312fc43d8f1a9fc6f10a34a568357184fa8019f3
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-02-01 11:54:18 -0300

    feat: orders by user admin

--- src/actions/order/get-orders-by-user.ts
+++ src/actions/order/get-orders-by-user.ts
@@ -12,6 +12,35 @@ export const getOrdersByUser = async () => {
       };
     }
 
+    // Si el role del usuario es admin, se puede obtener todas las ordenes
+    if (session.user.role === 'admin') {
+      const orders = await prisma.order.findMany({
+        include: {
+          OrderAddress: {
+            select: {
+              firstName: true,
+              lastName: true,
+            },
+          },
+        },
+      });
+      if (!orders) {
+        throw 'No hay ordenes';
+      }
+      const orderWithName = orders.map((order) => {
+        const { OrderAddress, ...restOrder } = order;
+        return {
+          ...restOrder,
+          name: OrderAddress!.firstName + ' ' + OrderAddress!.lastName,
+        };
+      });
+      return {
+        ok: true,
+        orders: orderWithName,
+      };
+    }
+
+    // Si el role del usuario es usuario, se puede obtener solo las ordenes del usuario actual
     const orders = await prisma.order.findMany({
       where: {
         userId: session.user.id,
