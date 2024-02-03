54b4083bdd298afa5275f980b95c05eb043a5f5a
Imanol Dominguez Sanchez <imanol.desarrolloweb@gmail.com>
2024-02-03 20:51:45 -0300

    feat: server action orders admin

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -11,6 +11,7 @@ export * from './countries/get-countries';
 export * from './order/place-order';
 export * from './order/get-order-by-id';
 export * from './order/get-orders-by-user';
+export * from './order/get-paginated-orders';
 
 export * from './payments/set-transaction-id';
 export * from './payments/paypal-check-payment';
---
+++ src/actions/order/get-paginated-orders.ts
@@ -0,0 +1,43 @@
+'use server';
+
+import { auth } from '@/auth.config';
+import prisma from '@/lib/prisma';
+export const getPagiinatedOrders = async () => {
+  try {
+    const session = await auth();
+    if (session?.user.role !== 'admin') {
+      return {
+        ok: false,
+        message: 'El usuario no es administrador',
+      };
+    }
+
+    const orders = await prisma.order.findMany({
+      orderBy: {
+        createdAt: 'desc',
+      },
+      include: {
+        OrderAddress: {
+          select: {
+            firstName: true,
+            lastName: true,
+          },
+        },
+      },
+    });
+    if (!orders) {
+      throw 'No hay ordenes';
+    }
+
+    return {
+      ok: true,
+      orders: orders,
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      ok: false,
+      message: 'No hay ordenes',
+    };
+  }
+};
