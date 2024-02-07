81fdf26521b968e79819782ff8282b6ec9eeda03
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-02-07 14:53:41 -0300

    style profile page

--- src/app/(shop)/profile/page.tsx
+++ src/app/(shop)/profile/page.tsx
@@ -1,4 +1,5 @@
 import { auth } from '@/auth.config';
+import Image from 'next/image';
 import { redirect } from 'next/navigation';
 
 export default async function ProfilePage() {
@@ -9,9 +10,45 @@ export default async function ProfilePage() {
     redirect('/');
   }
 
+  const { email, name, role, emailVerifed, image } = session.user;
+
   return (
-    <div>
-      <pre>{JSON.stringify(session?.user, null, 2)}</pre>
+    <div className="flex h-[80vh] w-full items-center justify-center">
+      <div className="max-w-md rounded-lg bg-white p-5 shadow-xl">
+        <div className="p-2">
+          <Image
+            width={100}
+            height={100}
+            className="mx-auto h-32 w-32 rounded-full"
+            src={image ?? '/imgs/placeholder.jpg'}
+            alt="John Doe"
+          />
+        </div>
+        <div className="p-2">
+          <h3 className="text-center text-xl font-medium leading-8 text-gray-900">
+            {name}
+          </h3>
+
+          <table className="text-md my-3">
+            <tbody>
+              <tr>
+                <td className="px-2 py-2 font-semibold text-gray-500">Rol</td>
+                <td className="px-2 py-2 capitalize">{role}</td>
+              </tr>
+              <tr>
+                <td className="px-2 py-2 font-semibold text-gray-500">Email</td>
+                <td className="px-2 py-2">{email}</td>
+              </tr>
+              <tr>
+                <td className="px-2 py-2 font-semibold text-gray-500">
+                  Email Verificado
+                </td>
+                <td className="px-2 py-2">{emailVerifed ? 'Si' : 'No'}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
