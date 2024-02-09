8d80e288b8e829309dfe33324affe0797d53d69b
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-02-09 15:16:16 -0300

    add speedinsights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import { inter } from '@/config/fonts';
 
 import './globals.css';
@@ -21,6 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Providers>{children}</Providers>
+        <SpeedInsights />
       </body>
     </html>
   );
