3f39a3df699b5d4e8b04a0ebf2ad322fed45ed5a
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-02-14 20:02:35 -0300

    fix: estilos dark profile

--- src/app/(shop)/profile/page.tsx
+++ src/app/(shop)/profile/page.tsx
@@ -21,7 +21,7 @@ export default async function ProfilePage() {
 
   return (
     <div className="flex h-[80vh] w-full items-center justify-center">
-      <div className="max-w-md rounded-lg bg-white p-5 shadow-xl">
+      <div className="dark:bg-dark-second max-w-md rounded-lg bg-gray-200 p-5 shadow-xl">
         <div className="p-2">
           <Image
             width={100}
@@ -32,24 +32,20 @@ export default async function ProfilePage() {
           />
         </div>
         <div className="p-2">
-          <h3 className="text-center text-xl font-medium leading-8 text-gray-900">
-            {name}
-          </h3>
+          <h3 className="text-center text-xl font-medium leading-8 ">{name}</h3>
 
           <table className="text-md my-3">
             <tbody>
               <tr>
-                <td className="px-2 py-2 font-semibold text-gray-500">Rol</td>
+                <td className="px-2 py-2 font-semibold ">Rol</td>
                 <td className="px-2 py-2 capitalize">{role}</td>
               </tr>
               <tr>
-                <td className="px-2 py-2 font-semibold text-gray-500">Email</td>
+                <td className="px-2 py-2 font-semibold ">Email</td>
                 <td className="px-2 py-2">{email}</td>
               </tr>
               <tr>
-                <td className="px-2 py-2 font-semibold text-gray-500">
-                  Email Verificado
-                </td>
+                <td className="px-2 py-2 font-semibold ">Email Verificado</td>
                 <td className="px-2 py-2">{emailVerifed ? 'Si' : 'No'}</td>
               </tr>
             </tbody>
