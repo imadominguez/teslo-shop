3540b027cf31cffbed9a6e3794b6005352491bb6
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-02-17 09:43:16 -0300

    fix: estilos title component

--- src/components/ui/title/Title.tsx
+++ src/components/ui/title/Title.tsx
@@ -10,16 +10,16 @@ interface Props {
 export const Title = ({ title, subtitle, className }: Props) => {
   return (
     // Contenedor principal del título con posibilidad de aplicar una clase personalizada
-    <div className={`${className} mt-3`}>
+    <div className={`${className ? className : ''} my-3`}>
       {/* Título principal con estilos de fuente específicos */}
       <h1
-        className={`${font.className} mb-3 mt-7 text-2xl font-semibold antialiased md:text-4xl`}
+        className={`${font.className} mb-3 text-lg font-semibold antialiased md:text-2xl`}
       >
         {title}
       </h1>
 
       {/* Subtítulo opcional con estilos de fuente específicos */}
-      {subtitle && <h3 className="mb-5 text-lg md:text-xl">{subtitle}</h3>}
+      {subtitle && <h3 className="text-sm md:text-xl">{subtitle}</h3>}
     </div>
   );
 };
